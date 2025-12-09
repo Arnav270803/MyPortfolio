38880157daf117545649612a4881bed726e7d201
Arnav Sharma <123370153+Arnav270803@users.noreply.github.com>
2025-12-09 18:52:39 +0530

    Added chatbot icon

--- src/pages/Homee.jsx
+++ src/pages/Homee.jsx
@@ -12,6 +12,19 @@ const Homee = ({isDark  , toggleTheme ,setIsDark }) => {
   return (
     <div>
       <div className={`${isDark ? 'bg-neutral-900 text-white' : 'bg-zinc-50 text-gray-900'} min-h-screen transition-colors duration-300`}>
+      
+      <div className='fixed bottom-4 right-4 z-50 flex gap-2'>
+        <lord-icon
+          src="https://cdn.lordicon.com/uyxrgiem.json"
+          trigger="loop"
+          state="loop-cycle"
+          colors={isDark ? "primary:#b4b4b4,secondary:#ffffff" : "primary:#b4b4b4,secondary:#000000"}
+          style={{width: '60px', height: '60px'}}>
+        </lord-icon>
+        
+
+      </div>
+
         <div className={`max-w-3xl border-l border-r ${isDark ? 'border-gray-800' : 'border-neutral-200'} mx-auto  py-5`}>
          
           <div className='mt-4'>    
